c6fee2752431685c5ca38b8f79f42b829393d6f6
RahulKaushik7 <147498134+RahulKaushik7@users.noreply.github.com>
2025-04-03 22:08:02 +0530

    middle container change

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,13 +1,13 @@
 const ContactUs = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-4">
-            <h1 className="text-2xl font-bold mb-4">Let’s talk about deepfakes!</h1>
-            <div className="bg-gray-100 p-4 rounded-lg shadow-md text-center max-w-lg">
-                <p className="text-gray-700">Whether it's for business or technical purposes.</p>
-                <p className="text-gray-700">Don't hesitate to reach out to us – we would love to hear from you.</p>
-                <p className="text-gray-700">
+        <div className="flex flex-col items-center justify-center min-h-screen p-6">
+            <h1 className="text-3xl font-bold mb-6">Let’s talk about deepfakes!</h1>
+            <div className="bg-gray-100 p-6  shadow-lg text-center w-11/12 max-w-screen-lg">
+                <p className="text-gray-700 text-lg mb-2">Whether it's for business or technical purposes.</p>
+                <p className="text-gray-700 text-lg mb-4">Don't hesitate to reach out to us – we would love to hear from you.</p>
+                <p className="text-gray-700 text-lg">
                     You can contact us by sending an email to 
-                    <a href="mailto:info@deepware.ai" className="text-blue-500 font-semibold"> info@deepware.ai</a>
+                    <a href="mailto:info@deepware.ai" className="text-blue-600 font-semibold"> info@deepware.ai</a>
                 </p>
             </div>
         </div>
